f6533f4d557963cd0c766d30f6b112de4d8467be
Ricardo Trujillo <rtrujillor@hotmail.com>
2016-06-08 13:16:02 +0200

    inital commit

---
+++ eventsModule.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Consultant on 07/06/2016.
+ */
+var events = (function(){
+  var topics = {}
+      hOP = topics.hasOwnProperty;
+
+  return {
+    subscribe: function(topic, listener) {
+
+      if(!topics.hasOwnProperty(topic)){
+        topics[topic] = [];
+      }
+
+      var index = topics[topic].push(listener) -1;
+
+      return {
+        remove : function() {
+          delete topics[topic][index];
+        }
+      };
+    },
+
+    publish: function(topic, info){
+
+      if(!topics.hasOwnProperty(topic)) return;
+      if(!hOP.call(topics, topic)) return;
+
+      topics[topic].forEach( function( listener ) {
+        listener(info);
+        listener(info != undefined ? info : {});
+      })
+    }
+  }
+
+}());
+
+events.publish('topic1', 'info for topic1');
+events.publish('topic2', 'info for topic2');
+events.publish('topic2', 'another info for topic2 ');
+
+var subscription1 = events.subscribe('topic1',function(data){
+  console.log('Data received : ' + data );
+});
+
+subscription1.remove();
